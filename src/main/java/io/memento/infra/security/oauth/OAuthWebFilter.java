d9ed68b78dfb71f1ebf935a764833b53f8c34955
Jérémy Buget <jbuget@agile-spirit.fr>
2014-09-02 18:20:52 +0200

    Open service /api/accounts

--- src/main/java/io/memento/infra/security/oauth/OAuthWebFilter.java
+++ src/main/java/io/memento/infra/security/oauth/OAuthWebFilter.java
@@ -40,7 +40,9 @@ public class OAuthWebFilter extends HttpHeadersAccessor implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        if ("OPTIONS".equals(request.getMethod()) || request.getRequestURI().equals("/api/connection/login")) {
+        if ("OPTIONS".equals(request.getMethod())
+                || request.getRequestURI().equals("/api/connection/login")
+                || request.getRequestURI().equals("/api/accounts/")) {
             /*
              * In case of preflight (OPTIONS method) or connection requests, we do nothing.
              * In all other cases, requests should have an access token into their headers.
